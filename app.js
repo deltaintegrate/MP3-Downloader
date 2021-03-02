dff7046441f35cf9eaddf3c057b69e5ed61f2a40
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2021-03-01 23:10:03 -0500

    agregando MERV

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
-//verify the arg input
-var arg = '';
-for (let j = 0; j < process.argv.length; j++) {
-arg = process.argv[j];
-}
+// var Downloader = require("./src/components/Downloader");
+// var dl = new Downloader();
+// var i = 0;
 
-var YoutubeMp3Downloader = require("youtube-mp3-downloader");
+// //verify the arg input
+// var arg = '';
+// for (let j = 0; j < process.argv.length; j++) {
+// arg = process.argv[j];
+// }
 
-var YD = new YoutubeMp3Downloader({
-  "ffmpegPath": "/usr/local/bin/ffmpeg",        // FFmpeg binary location
-  "outputPath": "./src/output",    // Output file location (default: the home directory)
-  "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
-  "queueParallelism": 2,                  // Download parallelism (default: 1)
-  "progressTimeout": 2000,                // Interval in ms for the progress reports (default: 1000)
-  "allowWebm": false                      // Enable download from WebM sources (default: false)
+// console.log(arg);
+
+// var url = require('url');
+// var url_parts = url.parse(arg, true);
+// var query = JSON.parse(JSON.stringify(url_parts.query));
+
+// dl.getMP3({videoId: query["v"]}, function(err,res){
+//     i++;
+//     if(err)
+//         throw err;
+//     else{
+//         console.log("Song "+ i + " was downloaded: " + res.file);
+//     }
+// });
+
+
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+
+// Middleware
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
-//Download video and save as MP3 file
-YD.download(arg);
-YD.on("finished", function(err, data) {
-console.log(JSON.stringify(data));
-});
-YD.on("error", function(error) {
-console.log(error);
-});
-YD.on("progress", function(progress) {
-console.log(JSON.stringify(progress));
+
+// Middleware para Vue.js router modo history
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('puerto', process.env.PORT || 3000);
+app.listen(app.get('puerto'), () => {
+  console.log('Example app listening on port'+ app.get('puerto'));
 });
