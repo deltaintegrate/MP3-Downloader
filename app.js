4dbe9471abffbd21b811a0c521ac5923d2272cf6
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2021-03-01 19:04:50 -0500

    agregando valueid de youtube para descarga

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
+//verify the arg input
+var arg = '';
+for (let j = 0; j < process.argv.length; j++) {
+arg = process.argv[j];
+}
+
 var YoutubeMp3Downloader = require("youtube-mp3-downloader");
 
-//Configure YoutubeMp3Downloader with your settings
 var YD = new YoutubeMp3Downloader({
-    "ffmpegPath": "/ffmpeg",        // FFmpeg binary location
-    "outputPath": "./src/output",    // Output file location (default: the home directory)
-    "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
-    "queueParallelism": 2,                  // Download parallelism (default: 1)
-    "progressTimeout": 2000,                // Interval in ms for the progress reports (default: 1000)
-    "allowWebm": false                      // Enable download from WebM sources (default: false)
+  "ffmpegPath": "/usr/local/bin/ffmpeg",        // FFmpeg binary location
+  "outputPath": "./src/output",    // Output file location (default: the home directory)
+  "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
+  "queueParallelism": 2,                  // Download parallelism (default: 1)
+  "progressTimeout": 2000,                // Interval in ms for the progress reports (default: 1000)
+  "allowWebm": false                      // Enable download from WebM sources (default: false)
 });
-
 //Download video and save as MP3 file
-YD.download("Vhd6Kc4TZls");
-
+YD.download(arg);
 YD.on("finished", function(err, data) {
-    console.log(JSON.stringify(data));
+console.log(JSON.stringify(data));
 });
-
 YD.on("error", function(error) {
-    console.log(error);
+console.log(error);
 });
-
 YD.on("progress", function(progress) {
-    console.log(JSON.stringify(progress));
+console.log(JSON.stringify(progress));
 });
